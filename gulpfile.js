32d6227425362ad4c87871dbfdbe629af3458ebd
virgomessier104 <virgomessier104@protonmail.com>
2023-12-19 00:35:51 +0400

    V.1, starter pack for creating my portfolio site

---
+++ gulpfile.js
@@ -0,0 +1,63 @@
+const {src, dest, watch, parallel, series} = require('gulp');
+
+const scss = require('gulp-sass')(require ('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+function scripts() {
+    return src('src/js/main.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('src/js'))
+        .pipe(browserSync.stream())
+}
+
+function styles() {
+    return src('src/scss/style.scss')
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version']}))
+        .pipe(concat('style.min.css'))
+        .pipe(scss({ outputStyle: 'compressed' }))
+        .pipe(dest('src/css'))
+        .pipe(browserSync.stream())
+
+}
+
+function watching() {
+    watch(['src/scss/style.scss'], styles)
+    watch(['src/js/main.js'], scripts)
+    watch(['src/**/*.html']).on('change', browserSync.reload)
+
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "src/"
+        }
+    });
+}
+
+function cleanDist() {
+    return src('dist')
+    .pipe(clean())
+}
+
+function building() {
+    return src([
+        'src/css/style.min.css',
+        'src/js/main.min.js',
+        'src/**/*.html',
+    ], {base : 'src'})
+        .pipe(dest('dist'))
+}
+
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, browsersync, watching);
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+/*jslint devel: true, browser: true, white: true*/
+/*jslint maxerr: 10, es6, node, single, for, bitwise, for, multivar*/
+/*eslint-env es6, es2017, es2018*/
+/*eslint-disable no-console,*/
+/*global console*/
