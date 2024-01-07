95ba6945e9f0dc9fecdda50a89b2d9cc650a7af7
virgomessier104 <virgomessier104@protonmail.com>
2024-01-08 00:56:29 +0400

    V3.3 Remade portfolio page, add swiper and my real projects

--- src/js/main.js
+++ src/js/main.js
@@ -63,6 +63,54 @@ const counters = document.querySelectorAll('.skills__grade-item-counter'),
 	}
 	}(typeof module === 'undefined' ? window : module.exports);
 
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+		dynamicBullets: true,
+	},
+	
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+
+	slidesPerView: 1,
+
+	speed: 700,
+	spaceBetween: 100,
+
+	autoplay: {
+		delay: 3000,
+	},
+
+	// effect: 'coverflow',
+
+	// CoverflowEffect: {
+	// 	rotate: 20,
+	// 	stretch: 50,
+	// },
+
+	keyboard: {
+		enable: true,
+		onlyInViewport: true,
+		PageUpDown: false,
+	},
+
+});
+
+
 $(document).ready(function () {
 
 	$(window).scroll(function() {
