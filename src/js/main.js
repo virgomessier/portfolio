d141db2cb9a4cd95d7d67d93bb29ae3cbf988a50
virgomessier104 <virgomessier104@protonmail.com>
2024-01-25 03:22:56 +0400

    V3.7 add proxy for CORS issue

--- src/js/main.js
+++ src/js/main.js
@@ -35,34 +35,6 @@ const counters = document.querySelectorAll('.skills__grade-item-counter'),
 		scale[i].style.width = item.innerHTML;
 	});
 
-
-
-!function(exports) {
-	exports.submitGoogleForm = submitGoogleForm;
-	
-	function submitGoogleForm(form) {
-		try {
-		var data = [].slice.call(form).map(function(control) {
-			return 'value' in control && control.name ?
-			control.name + '=' + (control.value === undefined ? '' : control.value) :
-			'';
-		}).join('&');
-		var xhr = new XMLHttpRequest();
-	
-		xhr.open('POST', 'Access-Control-Allow-Origin: *', form.action + '/formResponse', true);
-		xhr.setRequestHeader('Accept',
-			'application/xml, text/xml, */*; q=0.01');
-		xhr.setRequestHeader('Content-type',
-			'application/x-www-form-urlencoded; charset=UTF-8');
-		xhr.send(data);
-		} catch(e) {}
-	
-		form.parentNode.className += ' submitted';
-	
-		return false;
-	}
-	}(typeof module === 'undefined' ? window : module.exports);
-
 const swiper = new Swiper('.swiper', {
 	// Optional parameters
 	direction: 'horizontal',
@@ -145,3 +117,30 @@ $(document).ready(function () {
 
 
 });
+
+
+!function(exports) {
+	exports.submitGoogleForm = submitGoogleForm;
+	
+	function submitGoogleForm(form) {
+		try {
+		var data = [].slice.call(form).map(function(control) {
+			return 'value' in control && control.name ?
+			control.name + '=' + (control.value === undefined ? '' : control.value) :
+			'';
+		}).join('&');
+		var xhr = new XMLHttpRequest();
+	
+		xhr.open('POST', form.action + '/formResponse', true);
+		xhr.setRequestHeader('Accept',
+			'application/xml, text/xml, */*; q=0.01');
+		xhr.setRequestHeader('Content-type',
+			'application/x-www-form-urlencoded; charset=UTF-8');
+		xhr.send(data);
+		} catch(e) {}
+	
+		form.parentNode.className += ' submitted';
+	
+		return false;
+	}
+	}(typeof module === 'undefined' ? window : module.exports);
